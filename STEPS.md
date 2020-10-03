0c308f9b03f1ee80a665c09af9637845425c8607
Gullies <1448991007@qq.com>
2020-10-04 04:49:25 +0800

    Modify the configuration file config.rb

---
+++ STEPS.md
@@ -0,0 +1,11 @@
+### STEPS
+
+## 
+```bash
+# Modify the configuration file config.rb
+# css_dir = "css"
+css_dir = "..\\heroku-ruby-boilerplate\\react_client\\src\\components\\ui"
+
+compass watch
+
+```
--- config.rb
+++ config.rb
@@ -3,7 +3,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+# css_dir = "css"
+css_dir = "..\\heroku-ruby-boilerplate\\react_client\\src\\components\\ui"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
